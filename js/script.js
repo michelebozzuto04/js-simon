2f07d6ab0437b56c0cdeff9d01bfbd09aae34d0c
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-09 15:57:56 +0200

    Fix countdown timer functionality and ensure number list is populated dynamically

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Get Elements from he DOM
 const timerEl = document.getElementById("timer");
 const countdownEl = document.getElementById("countdown");
+const numberListEl = document.getElementById("numbers-list");
 const instructionsEl = document.getElementById("instructions");
 const answersFormEl = document.getElementById("answers-form");
 const inputConfirmBtn = document.querySelector("#answers-form button");
@@ -27,10 +28,11 @@ function generateRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Load list items with numbers dynamically
 for (let i = 0; i < 5; i++) {
     randomNumbers[i] = generateRandom(1, 50);
+    numberListEl.innerHTML += `<li>${randomNumbers[i]}</li>`
 }
 
 // Show Output
 countdownTimer(30);
-console.log(randomNumbers);
